d786d88af496ddd1f1fbda1d0bc2762985447c69
Brady Robshaw <heybrady88@gmail.com>
2022-04-05 11:05:47 -0400

    Initial Commit

---
+++ Paper Dice/Paper_DiceApp.swift
@@ -0,0 +1,20 @@
+//
+//  Paper_DiceApp.swift
+//  Paper Dice
+//
+//  Created by Brady Robshaw on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Paper_DiceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
